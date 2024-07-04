56498527908f502d1b8355a5472b44eb35e1d185
nullSector <rlatmd017@gmail.com>
2024-07-05 01:53:24 +0900

    [feat] Step-05 장바구니 관련 Mock API 추가
    - 장바구니 관련 controller 추가
    - 관련 ResponseDto 추가
    - FakeDto 추가

---
+++ src/main/java/com/hhplus/assignment/ecommerce/cart/controller/CartController.java
@@ -0,0 +1,30 @@
+package com.hhplus.assignment.ecommerce.cart.controller;
+
+import com.hhplus.assignment.ecommerce.cart.request.dto.CartRequestDto;
+import com.hhplus.assignment.ecommerce.cart.response.CartListResponse;
+import com.hhplus.assignment.ecommerce.cart.response.FakeCardResponse;
+import com.hhplus.assignment.ecommerce.common.genericResponse.DataResponse;
+import com.hhplus.assignment.ecommerce.common.genericResponse.GenericResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/cart")
+public class CartController {
+
+    private final FakeCardResponse fakeCardResponse;
+
+    @GetMapping("/{memberId}")
+    public GenericResponse getCartList(@PathVariable("memberId") Long memberId) {
+        return CartListResponse.create(fakeCardResponse.cartResponseDtoList);
+    }
+
+    @PostMapping
+    public GenericResponse addProductToCart(@RequestBody CartRequestDto requestDto) {
+        log.info("카트에 상품 추가/제거 요청 : {}", requestDto);
+        return DataResponse.create("카트 업데이트 성공");
+    }
+}
---
+++ src/main/java/com/hhplus/assignment/ecommerce/cart/response/dto/CartResponseDto.java
@@ -0,0 +1,19 @@
+package com.hhplus.assignment.ecommerce.cart.response.dto;
+
+import com.hhplus.assignment.ecommerce.cart.domain.entity.CartEntity;
+
+import java.time.LocalDateTime;
+
+public record CartResponseDto(
+        Long memberId,
+        Long productOptionId,
+        String productName,
+        String productOptionName,
+        Integer quantity,
+        Integer price,
+        LocalDateTime createdAt
+) {
+    public CartResponseDto(CartEntity cartEntity, String productName, String productOptionName, Integer price) {
+        this(cartEntity.getMemberId(), cartEntity.getProductOptionId(), productName, productOptionName, cartEntity.getQuantity(), cartEntity.getQuantity()*price, cartEntity.getCreatedAt());
+    }
+}
