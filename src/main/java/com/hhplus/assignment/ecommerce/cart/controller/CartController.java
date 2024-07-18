e6e66c9ebf72eb879991feed3b3b9ed3782932ba
nullSector <rlatmd017@gmail.com>
2024-07-19 07:20:36 +0900

    [fix] 더미파일 제거
    - cart, erd 등 빌드에러 발생시키거나 필요없는 파일 제거

--- src/main/java/com/hhplus/assignment/ecommerce/cart/controller/CartController.java
+++ src/main/java/com/hhplus/assignment/ecommerce/cart/controller/CartController.java
@@ -2,7 +2,6 @@ package com.hhplus.assignment.ecommerce.cart.controller;
 
 import com.hhplus.assignment.ecommerce.cart.request.dto.CartRequestDto;
 import com.hhplus.assignment.ecommerce.cart.response.CartListResponse;
-import com.hhplus.assignment.ecommerce.cart.response.FakeCardResponse;
 import com.hhplus.assignment.ecommerce.common.genericResponse.DataResponse;
 import com.hhplus.assignment.ecommerce.common.genericResponse.GenericResponse;
 import lombok.RequiredArgsConstructor;
@@ -15,16 +14,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/cart")
 public class CartController {
 
-    private final FakeCardResponse fakeCardResponse;
-
-    @GetMapping("/{memberId}")
-    public GenericResponse getCartList(@PathVariable("memberId") Long memberId) {
-        return CartListResponse.create(fakeCardResponse.cartResponseDtoList);
-    }
-
-    @PostMapping
-    public GenericResponse addProductToCart(@RequestBody CartRequestDto requestDto) {
-        log.info("카트에 상품 추가/제거 요청 : {}", requestDto);
-        return DataResponse.create("카트 업데이트 성공");
-    }
+//    private final FakeCardResponse fakeCardResponse;
+//
+//    @GetMapping("/{memberId}")
+//    public GenericResponse getCartList(@PathVariable("memberId") Long memberId) {
+//        return CartListResponse.create(fakeCardResponse.cartResponseDtoList);
+//    }
+//
+//    @PostMapping
+//    public GenericResponse addProductToCart(@RequestBody CartRequestDto requestDto) {
+//        log.info("카트에 상품 추가/제거 요청 : {}", requestDto);
+//        return DataResponse.create("카트 업데이트 성공");
+//    }
 }
